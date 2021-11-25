23f53d06c34a904b94fe5cc523e434393be48221
alamshafil <36016500+alamshafil@users.noreply.github.com>
2021-11-25 17:27:30 -0500

    New dns seeders

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -186,13 +186,8 @@ public:
         assert(consensus.hashGenesisBlock == uint256S("0x000000cdb10fc01df7fba251f2168ef7cd7854b571049db4902c315694461dd0"));
         assert(genesis.hashMerkleRoot == uint256S("0x63d9b6b6b549a2d96eb5ac4eb2ab80761e6d7bffa9ae1a647191e08d6416184d"));
 
-        // DNS Seeds
-       	vSeeds.emplace_back("159.65.178.148", true);
-	    vSeeds.emplace_back("144.91.77.184", true);
-        vSeeds.emplace_back("51.89.166.31", true);			    
-        vSeeds.emplace_back("66.191.202.105", true);
-	    vSeeds.emplace_back("144.202.0.55", true);
-	    vSeeds.emplace_back("71.202.82.78", true);
+        vSeeds.emplace_back("dnsseed.us.avn.network:7895", true);
+        vSeeds.emplace_back("dnsseed.eu.avn.network:7895", true);
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,60);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,122);
