15385990e749af9219830f42deb53f9d0b53b571
Xangis <jason@peopledatalabs.com>
2021-12-10 00:37:27 -0800

    Diff search and replace -- keep default value so builds without github actions work fine.

--- build_version_update.sh
+++ build_version_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Used to update the build number using the GitHub build number passed as a commandline argument.
 echo "GitHub build number provided as $1";
-sed -i -e "s/INSERTBUILDNUMBERHERE/$1/g" configure.ac
+sed -i -e "s/_CLIENT_VERSION_BUILD, 0/_CLIENT_VERSION_BUILD, $1/g" configure.ac
