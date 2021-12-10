3d0692cfc26ff83762fb19678a88e840afb12117
Xangis <jason@peopledatalabs.com>
2021-12-10 00:27:28 -0800

    Set build number in configure.ac because the PACKAGE_VERSION in the header file is generated from that.

--- build_version_update.sh
+++ build_version_update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Used to update the build number using the GitHub build number passed as a commandline argument.
 echo "GitHub build number provided as $1";
-BUILD="3.0.$1"
-echo "Setting build to $BUILD";
-sed -i -e "s/3.0.INSERTVERSIONHERE/$BUILD/g" src/config/avian-config.h
+sed -i -e "s/INSERTBUILDNUMBERHERE/$1/g" configure.ac
