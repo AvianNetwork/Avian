466dd4a336f078dc93447f1d67eda6b45fa31b68
Xangis <jason@peopledatalabs.com>
2021-12-10 01:44:23 -0800

    Revert placeholder text because we're doing it in configure.

--- src/config/avian-config.h
+++ src/config/avian-config.h
@@ -347,7 +347,7 @@
 #define PACKAGE_NAME "Avian Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Avian Core 3.0.INSERTVERSIONHERE"
+#define PACKAGE_STRING "Avian Core 3.0.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "avian"
@@ -356,7 +356,7 @@
 #define PACKAGE_URL "https://avian.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.0.INSERTVERSIONHERE"
+#define PACKAGE_VERSION "3.0.3"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
