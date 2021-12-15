a598f488ff911f8794fdebbfb16ddf7e81d0c8ca
fdov <fdoving@gmail.com>
2021-12-15 19:28:10 +0100

    depends: update expat to 2.4.1 (#31)
    CVE-2019-15903
    CVE-2018-20843
    
    (cherry picked from commit d634ccddc3aa51a57c533ed1d78674d97255d7b1)

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.2.6
-$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_2_2_6/
+$(package)_version=2.4.1
+$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$($(package)_version))/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2
+$(package)_sha256_hash=2f9b6a580b94577b150a7d5617ad4643a4301a6616ff459307df3e225bcfbf40
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static --without-docbook
