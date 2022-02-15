ff2d11b9fd9e0bafb23aa704c53b097c2ca93cc8
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-02-14 21:48:53 -0500

    Remove unneeded include

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -4,8 +4,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <iostream>
-
 #include "overviewpage.h"
 #include "ui_overviewpage.h"
 
