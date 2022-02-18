a340fcbf79e88a8de7ce546012010f60f14b43ed
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-02-18 18:45:50 -0500

    Depends: Update Boost to 1.78.0

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_66_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.66.0/source/
+$(package)_version=1_78_0
+$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=5721818253e6a0989583192f96782c4a98eb6204965316df9f5ad75819225ca9
+$(package)_sha256_hash=8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
