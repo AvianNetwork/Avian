811a916807ef0e202d201fb394ec9546715ffc58
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-02-21 15:59:02 -0500

    Flightplans: Use compat/endian.h

--- src/flightplans/keccak.cpp
+++ src/flightplans/keccak.cpp
@@ -6,11 +6,7 @@
 
 #include "keccak.h"
 
-// big endian architectures need #define __BYTE_ORDER __BIG_ENDIAN
-#ifndef _MSC_VER
-#include <endian.h>
-#endif
-
+#include "compat/endian.h"
 
 /// same as reset()
 Keccak::Keccak(Bits bits)
