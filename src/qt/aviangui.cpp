b17825adc51d1bd2494dfe0dbee768a01255f00f
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-02-25 21:41:02 -0500

    UI: Fix QApp font

--- src/qt/aviangui.cpp
+++ src/qt/aviangui.cpp
@@ -230,7 +230,6 @@ AvianGUI::AvianGUI(const PlatformStyle *_platformStyle, const NetworkStyle *netw
 
 #if !defined(Q_OS_MAC)
     this->setFont(QFont("Manrope"));
-    QApplication::setFont(QFont("Manrope"));
 #endif
 
     // Create actions for the toolbar, menu bar and tray/dock icon
