80d0934b1c096a28c14403863852972242039152
cdonnachie <craig.donnachie@gmail.com>
2022-05-04 09:12:54 -0400

    Another test

--- src/qt/aviangui.cpp
+++ src/qt/aviangui.cpp
@@ -167,7 +167,7 @@ AvianGUI::AvianGUI(const PlatformStyle *_platformStyle, const NetworkStyle *netw
 
 {
     GUIUtil::restoreWindowGeometry("MainWindowGeometry", QSize(850, 550), this);
-    this.setMinimumSize(600, 350);
+    setMinimumSize(600, 350);
 
     QString windowTitle = tr(PACKAGE_NAME) + " - ";
 #ifdef ENABLE_WALLET
