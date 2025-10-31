7f249af41c7af7c8c5242be4dc1a40d2759fce71
cdonnachie <craig.donnachie@gmail.com>
2025-10-30 20:35:07 -0400

    Fix build

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,7 +3,7 @@ $(package)_version=4.3.4
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=c593001a89f5a85dd2ddf564805deb860e02471171b3f204944857336295c3e5
-$(package)_patches=remove_libstd_link.patch
+$(package)_patches=remove_libstd_link.patch condition_variable_windows.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --disable-curve --disable-curve-keygen --disable-perf
