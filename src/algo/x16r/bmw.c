269b9dd0b94bb0793a2caf015fc43cb0f34eb4e9
cdonnachie <craig.donnachie@gmail.com>
2026-03-26 13:08:42 -0400

    x16r: bmw: zero-initialize context
    Zero the BMW small/big context structs in init so padding/buffer bytes are deterministic with newer compilers.

--- src/algo/x16r/bmw.c
+++ src/algo/x16r/bmw.c
@@ -613,6 +613,7 @@ static const sph_u32 final_s[16] = {
 static void
 bmw32_init(sph_bmw_small_context *sc, const sph_u32 *iv)
 {
+	memset(sc, 0, sizeof(sph_bmw_small_context));
 	memcpy(sc->H, iv, sizeof sc->H);
 	sc->ptr = 0;
 #if SPH_64
@@ -763,6 +764,7 @@ static const sph_u64 final_b[16] = {
 static void
 bmw64_init(sph_bmw_big_context *sc, const sph_u64 *iv)
 {
+	memset(sc, 0, sizeof(sph_bmw_big_context));
 	memcpy(sc->H, iv, sizeof sc->H);
 	sc->ptr = 0;
 	sc->bit_count = 0;
