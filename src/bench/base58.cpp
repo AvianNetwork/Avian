0f251e4a14b949ef614b9a6fddc67063edec22d4
cdonnachie <craig.donnachie@gmail.com>
2026-03-27 13:07:31 -0400

    bench: replace BTC address with Avian address in Base58Decode benchmark

--- src/bench/base58.cpp
+++ src/bench/base58.cpp
@@ -43,7 +43,7 @@ static void Base58CheckEncode(benchmark::Bench& bench)
 
 static void Base58Decode(benchmark::Bench& bench)
 {
-    const char* addr = "17VZNX1SN5NtKa8UQFxwQbFeFc3iqRYhem";
+    const char* addr = "rPC7kPCNPAVnUvQs4fWEvnFwJ4yfKvArXM";
     std::vector<unsigned char> vch;
     bench.batch(strlen(addr)).unit("byte").run([&] {
         (void) DecodeBase58(addr, vch, 64);
