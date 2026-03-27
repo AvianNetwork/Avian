624979061907519f9efd82a1846234fceddcf6ff
cdonnachie <craig.donnachie@gmail.com>
2026-03-27 13:04:06 -0400

    rpc: replace BTC example addresses with Avian P2PKH placeholders in EXAMPLE_ADDRESS

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -44,7 +44,7 @@ using util::SplitString;
 using util::TrimString;
 
 const std::string UNIX_EPOCH_TIME = "UNIX epoch time";
-const std::string EXAMPLE_ADDRESS[2] = {"bc1q09vm5lfy0j5reeulh4x5752q25uqqvz34hufdl", "bc1q02ad21edsxd23d32dfgqqsz4vv4nmtfzuklhy3"};
+const std::string EXAMPLE_ADDRESS[2] = {"RAvianNetXXXXXXXXXXXXXXXXXXXXXXXXX", "RAvianNet2XXXXXXXXXXXXXXXXXXXXXXXX"};
 
 std::string GetAllOutputTypes()
 {
