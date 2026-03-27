fecdac84e2b97998dbe3bd47298a589c978f0bc6
cdonnachie <craig.donnachie@gmail.com>
2026-03-27 07:58:00 -0400

    qt: replace BTC dummy addresses with Avian P2PKH placeholders in DummyAddress()

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -108,21 +108,21 @@ QFont fixedPitchFont(bool use_embedded_font)
     return QFontDatabase::systemFont(QFontDatabase::FixedFont);
 }
 
-// Return a pre-generated dummy bech32m address (P2TR) with invalid checksum.
+// Return a pre-generated dummy legacy (P2PKH) address with invalid checksum.
 static std::string DummyAddress(const CChainParams &params)
 {
     std::string addr;
     switch (params.GetChainType()) {
     case ChainType::MAIN:
-        addr = "bc1p35yvjel7srp783ztf8v6jdra7dhfzk5jaun8xz2qp6ws7z80n4tq2jku9f";
+        addr = "RAvianNetXXXXXXXXXXXXXXXXXXXXXXXXX";
         break;
     case ChainType::SIGNET:
     case ChainType::TESTNET:
     case ChainType::TESTNET4:
-        addr = "tb1p35yvjel7srp783ztf8v6jdra7dhfzk5jaun8xz2qp6ws7z80n4tqa6qnlg";
+        addr = "mAvianTestXXXXXXXXXXXXXXXXXXXXXXXX";
         break;
     case ChainType::REGTEST:
-        addr = "bcrt1p35yvjel7srp783ztf8v6jdra7dhfzk5jaun8xz2qp6ws7z80n4tqsr2427";
+        addr = "mAvianRegXXXXXXXXXXXXXXXXXXXXXXXXX";
         break;
     } // no default case, so the compiler can warn about missing cases
     assert(!addr.empty());
