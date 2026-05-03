7b9a96c0a3b9f8de9c69344318d411aed442533d
cdonnachie <craig.donnachie@gmail.com>
2026-05-03 17:01:53 -0400

    rpc: add DEPLOYMENT_MLDSA44 to getdeploymentinfo output

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1805,6 +1805,7 @@ UniValue DeploymentInfo(const CBlockIndex* blockindex, const ChainstateManager& 
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_SEGWIT);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TESTDUMMY);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TAPROOT);
+    SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_MLDSA44);
     return softforks;
 }
 } // anon namespace
